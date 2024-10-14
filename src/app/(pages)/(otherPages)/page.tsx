be23be9e89846ed1949a482909a64cf51642fcb1
sayedo2121 <sayedo2121@gmail.com>
2024-10-14 23:30:40 +0100

    added prefetch and loading page

--- src/app/(pages)/(otherPages)/page.tsx
+++ src/app/(pages)/(otherPages)/page.tsx
@@ -2,10 +2,8 @@
 
 import { useSoundContext } from "@/app/context/SoundContext";
 import Link from "next/link";
-import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
-
 import { useState } from "react";
 
 export default function rootPage() {
@@ -20,14 +18,14 @@ export default function rootPage() {
         setLoadingFinished(true);
       };
 
-      gsap.to(".loadingBar > div", {
-        width: "100%",
-        duration: 4,
-        ease: "power3.inOut(1.7)",
-        onComplete: onLoadingFinished,
-      });
-
-      if (loadingFinished) {
+      if (!loadingFinished) {
+        gsap.to(".loadingBar > div", {
+          width: "100%",
+          duration: 4,
+          ease: "power3.inOut(1.7)",
+          onComplete: onLoadingFinished,
+        });
+      } else {
         gsap.to(".startButton", {
           opacity: 1,
           ease: "power3.inOut(1.7)",
@@ -40,12 +38,16 @@ export default function rootPage() {
   return (
     <>
       <div className="w-screen h-screen flex flex-col items-center justify-center bg-black bg-gradient-to-tr from-sky-800/50 to-indigo-900/50 relative">
-       <p className="text-xs text-sky-500 font-light font-helvetica pointer-none">{loadingFinished? 'SPACE SUIT PRESSURISED': 'PRESSURISING YOUR SPACE SUIT...'}</p>
+        <p className="text-xs text-sky-500 font-light font-helvetica pointer-none">
+          {loadingFinished
+            ? "SPACE SUIT PRESSURISED"
+            : "PRESSURISING YOUR SPACE SUIT..."}
+        </p>
         <div className="loadingBar w-1/2 h-[1.5px] rounded-full bg-black mt-4 flex flex-row items-center justify-center">
           <div className="w-0 h-full rounded-full bg-sky-800"></div>
         </div>
-        { loadingFinished &&
-          <Link href={"/home"} className="absolute bottom-10 mt-32">
+        {loadingFinished && (
+          <Link href={"/home"} prefetch={true} className="absolute bottom-10 mt-32">
             <button
               onClick={soundOnFunction}
               className="startButton opacity-0 rounded-lg text-white px-10 py-2 border-2 border-sky-800/20 bg-gradient-to-tr from-sky-800/50 to-indigo-900/50"
@@ -53,7 +55,7 @@ export default function rootPage() {
               Explore
             </button>
           </Link>
-        }
+        )}
       </div>
     </>
   );
